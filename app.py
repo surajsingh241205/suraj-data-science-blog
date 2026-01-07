dcf34d7f6a7817972d5e8ffae1c85e78aa18cdb4
Suraj Singh <suraj.datascience24@gmail.com>
2026-01-07 13:27:23 +0530

    Fix port binding for render

--- app.py
+++ app.py
@@ -41,5 +41,9 @@ def post(slug):
     title = content.split('\n')[0].replace("#", "").strip()
     return render_template("post.html", title=title, content=html)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
